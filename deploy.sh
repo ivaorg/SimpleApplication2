6ac626fe2626772b0bc61b5bdd4073b248d12c03
x23184396 <x23184396@student.ncirl.ie>
2024-03-15 14:13:15 +0000

    changes

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# Install and update Node.js and npm
+sudo apt update && sudo apt install nodejs npm
+
+# Install pm2 - production process manager for Node.js
+sudo npm install -g pm2
+
+# Change directory to SimpleApplication
+cd SimpleApplication2/
+
+# Install all dependencies
+npm install
+
+# Save private key and the certificate
+echo $PRIVATE_KEY > privatekey.pem
+echo $SERVER > server.crt
+
+# Kill all running processes
+pm2 kill
+
+# Start the app using pm2
+pm2 start ./bin/www --name SimpleApplication2 --env production
